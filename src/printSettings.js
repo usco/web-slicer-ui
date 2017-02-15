9eabc6d21c3f0137cdb2aaed1cb6721b4160f0ac
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-02-15 16:30:27 +0100

    feat(printSettings): fleshed out printSettings: now overall ui structure matched the app

--- src/printSettings.js
+++ src/printSettings.js
@@ -15,6 +15,14 @@ const actions = {Toggle}
 
 const PrintSettings = ({t}) => {
 
+  const selectedSupportMaterial = 'pva'
+
+  const state = {
+    support: {toggled: true},
+    brim: {toggled: true}
+  }
+  const {support, brim} = state
+
   const layerHeights = [0.2, 0.15, 0.1, 0.06]
   const layerHeightNames = {
     '0.2': t('activity_print_settings_text_draft'),
@@ -25,10 +33,29 @@ const PrintSettings = ({t}) => {
   const layerHeightUnit = 'mm'
 
   const layerHeightButtons = layerHeights.map(function (height) {
-    const buttonText = `${layerHeightNames[height]}\n${height} ${layerHeightUnit}`
+    const buttonText = `${layerHeightNames[height]}
+${height} ${layerHeightUnit}`
     return <li>
-      <button>{buttonText}</button>
-    </li>
+             <button>
+               {buttonText}
+             </button>
+           </li>
+  })
+
+  const supportMaterials = ['pva', 'pla']
+
+  const supportMaterialsUi = supportMaterials.map(function (supportMaterial) {
+    return <div>
+             <input
+               type="radio"
+               name='supportMaterials'
+               value={supportMaterial}
+               checked={supportMaterial === selectedSupportMaterial}>
+             {supportMaterial}
+             </input>
+             Extruder 1:
+             {supportMaterial}
+           </div>
   })
 
   return <section className='printSettings'>
@@ -40,11 +67,33 @@ const PrintSettings = ({t}) => {
            </section>
            <section className='profile'>
              <header>
+               <h1>{t('activity_print_settings_text_print_profile')}</h1>
                {t('activity_print_settings_layer_height_description')}
              </header>
-             <ul>
-              {layerHeightButtons}
-             </ul>
+             <div className='layerHeights'>
+               <ul>
+                 {layerHeightButtons}
+               </ul>
+             </div>
+           </section>
+           <section className='supports'>
+             <header>
+               <h1>{t('activity_print_settings_text_support_structure')} <input type='checkbox' checked={support.toggled} /></h1>
+             </header>
+             <div disabled={support.toggled ? false : 'disabled'}>
+               {t('activity_print_settings_text_select_support_structure_description')}
+               <form>
+                 {supportMaterialsUi}
+               </form>
+             </div>
+           </section>
+           <section className='brim'>
+             <header>
+               <h1>{t('activity_print_settings_text_adhesion_brim')} <input type='checkbox' checked={brim.toggled} /></h1>
+             </header>
+             <div disabled={brim.toggled ? false : 'disabled'}>
+               {t('activity_print_settings_text_brim_description')}
+             </div>
            </section>
          </section>
 }
