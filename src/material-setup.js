bf388f1978bfbd424c423f35bdfde4fb72e7ddd1
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-02-15 09:52:19 +0100

    chore(): added all base files

---
+++ src/material-setup.js
@@ -0,0 +1,24 @@
+
+
+export default function(){
+  const header = {
+    valid: true,
+    message: `You have `
+  }
+
+ return <section>
+  <header>
+
+  </header>
+  <div>
+    The printer is currently loaded with
+  </div>
+
+  <div>
+
+  </div>
+
+
+ </section>
+
+}
