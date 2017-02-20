89e1b997a133a7a3cdc715724788e4341423a748
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-02-20 11:16:16 +0100

    feat(utils): added various cycle.js related utilities

---
+++ src/utils/cycle.js
@@ -0,0 +1,43 @@
+//import { of, merge } from 'most'
+import xs from 'xstream'
+const {of, merge} = xs
+import convert from 'stream-conversions'
+
+export function domEvent (sources, selector, event) {
+  return sources.DOM.select(selector).events(event)
+}
+
+export function makeStateAndReducers$ (actions$, actionFns, sources) {
+  const {init} = actionFns
+  const init$ = of(init)
+
+  const elements = Object.keys(actions$).map(function (actionName$) {
+    const name = actionName$.replace('Action$', '')
+    const actFn = actionFns[name]
+    const act$ = actions$[actionName$]
+      .map(action => state => actFn(state, action))
+    return act$
+  })
+
+  const reducer$ = merge(init$, ...elements)
+  const state$ = sources.onion.state$
+  return {state$, reducer$}
+}
+
+export function mergeReducers (init, components) {
+  return merge(of(init), ...components.map(x => x.onion)) // FIXME :toMost is only temporary
+}
+
+export function makeDefaultReducer (defaultstate = {}) {
+  return function defaultReducer (prevState) {
+    if (typeof prevState === 'undefined') {
+      return defaultstate // Parent didn't provide state for the child, so initialize it.
+    } else {
+      return prevState // Let's just use the state given from the parent.
+    }
+  }
+}
+
+const toMost = convert.xstream.to.most
+const fromMost = convert.most.to.xstream
+export { toMost, fromMost }
