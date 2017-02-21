83d65d82bbda588756426c44c412817b4287e49a
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-02-21 23:14:39 +0100

    feat(ui: monitorPrint): added basic layout for print monitor

--- src/ui/monitorPrint.js
+++ src/ui/monitorPrint.js
@@ -1,12 +1,39 @@
-import { h, render } from 'preact-cycle'
+import {section, div, button} from '@cycle/dom'
 import classNames from 'classnames'
-/** @jsx h */
+import {domEvent, makeStateAndReducers$} from '../utils/cycle'
 
-const MonitorPrint = ({state}) => {
+const init = () => ({ img: undefined, print: {status: 'paused'} })
+const startpause = (state, input) => ({...state, status: state.status === 'paused' ? 'running' : 'paused'})
+const abort = (state, input) => ({state})
 
-
-
+const actions = {
+  init,
+  startpause,
+  abort
 }
 
+const view = state => section([
+  div('', [
+    button('.startpause', state.paused ? 'play' : 'pause'),
+    button('.abort', 'abort')
+  ]),
+  img({props: {src: state.img}})
+])
+
+function MonitorPrint (sources) {
+  const _domEvent = domEvent.bind(null, sources)
+  const startpause$ = _domEvent('.startpause', 'click')
+  const abort$ = _domEvent('.abort', 'click')
+
+  const actions$ = {
+    startpause$, abort$
+  }
+  const {state$, reducer$} = makeStateAndReducers$(actions$, actions, sources)
+
+  return {
+    DOM: state$.map(view),
+    onion: reducer$
+  }
+}
 
 export default MonitorPrint
