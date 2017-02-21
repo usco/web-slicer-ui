279836079e642225f0ab9ae5c52b6f3092296d37
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-02-21 10:00:46 +0100

    refactor(viewer): cleaned up viewer basics/init

--- src/ui/viewer.js
+++ src/ui/viewer.js
@@ -1,6 +1,67 @@
-import reglm from 'regl'
+import reglM from 'regl'
 import {domEvent, fromMost, makeStateAndReducers$, makeDefaultReducer, toMost} from '../utils/cycle'
 import { h } from '@cycle/dom'
+import xs from 'xstream'
+
+
+
+const initRegl = (canvas) => {
+  return reglM({
+    canvas,
+    extensions: [
+      //  'oes_texture_float', // FIXME: for shadows, is it widely supported ?
+      // 'EXT_disjoint_timer_query'// for gpu benchmarking only
+    ],
+    profile: true,
+    attributes: {
+      alpha: false
+    }
+  })
+}
+
+function prepareRender (regl) {
+  const draw = regl({
+    frag: `
+  precision mediump float;
+  uniform vec4 color;
+  void main() {
+    gl_FragColor = color;
+  }`,
+
+    vert: `
+  precision mediump float;
+  attribute vec2 position;
+  uniform float angle;
+  uniform vec2 offset;
+  void main() {
+    gl_Position = vec4(
+      cos(angle) * position.x + sin(angle) * position.y + offset.x,
+      -sin(angle) * position.x + cos(angle) * position.y + offset.y, 0, 1);
+  }`,
+
+    attributes: {
+      position: [
+        0.5, 0,
+        0, 0.5,
+        1, 1]
+    },
+
+    uniforms: {
+  // the batchId parameter gives the index of the command
+      color: regl.prop('color'),
+      angle: ({tick}) => 0.01 * tick,
+      offset: regl.prop('offset')
+    },
+
+    depth: {
+      enable: false
+    },
+
+    count: 3
+  })
+  return draw
+}
+
 
 export default function GLComponent (sources) {
   const init = makeDefaultReducer({
@@ -27,58 +88,23 @@ export default function GLComponent (sources) {
       color: [0.96, 0.96, 0.96, 0.3]
     }
   })
-  const view = state => h('canvas', {hook: {insert: vnode => { regl = reglm({canvas: vnode.elm}) }}})
-
   let regl
   let render
+  const view = () => h('canvas', {
+    hook: {insert: vnode => { regl = initRegl(vnode.elm) }}
+  })
 
-  function prepareRender (regl) {
-    const draw = regl({
-      frag: `
-    precision mediump float;
-    uniform vec4 color;
-    void main() {
-      gl_FragColor = color;
-    }`,
-
-      vert: `
-    precision mediump float;
-    attribute vec2 position;
-    uniform float angle;
-    uniform vec2 offset;
-    void main() {
-      gl_Position = vec4(
-        cos(angle) * position.x + sin(angle) * position.y + offset.x,
-        -sin(angle) * position.x + cos(angle) * position.y + offset.y, 0, 1);
-    }`,
-
-      attributes: {
-        position: [
-          0.5, 0,
-          0, 0.5,
-          1, 1]
-      },
-
-      uniforms: {
-    // the batchId parameter gives the index of the command
-        color: regl.prop('color'),
-        angle: ({tick}) => 0.01 * tick,
-        offset: regl.prop('offset')
-      },
-
-      depth: {
-        enable: false
-      },
-
-      count: 3
-    })
-    return draw
+  const heartBeatAction$ = xs.periodic(5000)
+  const heartBeat = (state, input) => {
+    state = { ...state, v1: Math.cos(input * 0.05) }
+    return state
   }
 
-  const {state$, reducer$} = makeStateAndReducers$({}, {init}, sources)
+  const {state$, reducer$} = makeStateAndReducers$({heartBeatAction$}, {init, heartBeat}, sources)
 
   state$.addListener({next:
     function (state) {
+      // console.log('state here', state)
       if (regl) {
         if (!render) {
           render = prepareRender(regl)
@@ -91,7 +117,7 @@ export default function GLComponent (sources) {
     }
   })
 
-  /*visualState$
+  /* visualState$
     .thru(limitFlow(33))
     .tap(x => regl.poll())
     .tap(render)
@@ -99,7 +125,7 @@ export default function GLComponent (sources) {
       console.error('error in render', error)
       return just(null)
     })
-    .forEach(x => x)*/
+    .forEach(x => x) */
 
   return {
     DOM: state$.map(view),
