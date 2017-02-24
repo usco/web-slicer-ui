71856ed5bc1f23450b54a69af30772f1f32e5cb3
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-02-24 21:29:06 +0100

    feat(image utils): added conversion from blob to img url

--- src/utils/image.js
+++ src/utils/image.js
@@ -2,13 +2,26 @@
 export function formatImageData (inputType = 'uint8', outputType = 'base64', data) {
   const inputOutputFormater = {
     'uint8Tobase64': uint8ToBase64,
-    'hexTobase64': hexToBase64
+    'hexTobase64': hexToBase64,
+    'blobToimg': blobToImg
   }
 
   const formatter = inputOutputFormater[`${inputType}To${outputType}`]
-  data = formatter ? 'data:image/jpeg;base64,' + formatter(data) : data
+  if (inputType === 'blob') {
+    data = formatter ? formatter(data) : data
+  } else {
+    data = formatter ? 'data:image/jpeg;base64,' + formatter(data) : data
+  }
+
   return data
 }
+
+export function blobToImg (data) {
+  const urlCreator = window.URL || window.webkitURL
+  const imageUrl = urlCreator.createObjectURL(data)
+  return imageUrl
+}
+
 // data = URL.createObjectURL(data)
 export function uint8ToBase64 (str) {
   return btoa(String.fromCharCode.apply(null, str))
