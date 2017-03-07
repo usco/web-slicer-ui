5ca062f8eb0f20038bad275297a0ffdeede8c6ba
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-03-07 13:37:40 +0100

    feat(enityInfos): moved & update code from 'old jam' for entity infos handling - copied a portion of css to main css for now - also copied useful widgets code

---
+++ src/ui/entityInfos/index.js
@@ -0,0 +1,44 @@
+import {section, div, button, img} from '@cycle/dom'
+import {domEvent, makeStateAndReducers$} from '../../utils/cycle'
+import {renderPositionUi} from './position'
+import {renderRotationUi} from './rotation'
+
+const init = (state) => {
+  console.log('init entityInfos state', state)
+  return state
+}
+
+export const actions = {
+  init
+}
+
+const view = function (state) {
+  console.log('EntityInfos', state)
+  return div('EntityInfos',[
+    renderPositionUi(state),
+    renderRotationUi(state)
+  ])
+}
+/* section('.MonitorPrint', [
+  div('', [
+    button('.startpause', state.paused ? 'play' : 'pause'),
+    button('.abort', 'abort')
+  ]),
+  img('.printerCameraFrame', {props: {src: state.image ? state.image : ''}})
+]) */
+
+function EntityInfos (sources) {
+  const _domEvent = domEvent.bind(null, sources)
+
+  const actions$ = {
+    // startpause$, abort$
+  }
+  const {state$, reducer$} = makeStateAndReducers$(actions$, actions, sources)
+
+  return {
+    DOM: state$.map(view),
+    onion: reducer$
+  }
+}
+
+export default EntityInfos
