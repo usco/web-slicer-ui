474d7c7d46eab4a1c924ef6cfb9fd22657c596be
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-03-08 11:45:54 +0100

    feat(togglable tool mode): made toolmodes toogleable

--- src/core/entities/intents.js
+++ src/core/entities/intents.js
@@ -14,13 +14,19 @@ export default function intents (sources) {
   const addEntities$ = dataSources(sources)
     .tap(x => console.log('adding entities', x))
 
+  // every time a new entity gets added, select it
   const selectEntities$ = addEntities$
     .map(x => x.meta.id)
     .map(toArray)
     .tap(x => console.log('fooo entities', x))
 
+  // if we set the same tool twice, disable it (ie, toggle mode)
+  const setActiveTool$ = baseActions.setActiveTool$
+    .scan((current, newValue) => {
+      return current === newValue ? undefined : newValue
+    })
 
-  const refinedActions = {addEntities$, selectEntities$}
+  const refinedActions = {addEntities$, selectEntities$, setActiveTool$}
 
   return {...baseActions, ...refinedActions}
 }
