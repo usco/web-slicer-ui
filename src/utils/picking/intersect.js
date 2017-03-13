9fce0ec0173e63f15510141ef93170759138f36c
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-03-13 14:35:00 +0100

    feat(intersect): modified default behaviour of intersects: - now limiting to aabb testing by default - modified returned data structures - minor tweaks & hacks

--- src/utils/picking/intersect.js
+++ src/utils/picking/intersect.js
@@ -8,7 +8,7 @@ import intersectTRI from 'ray-triangle-intersection'
  * compute intersection point(s) between a ray and an entity
  * @param  {Object} ray the picking ray IN WORLD SPACE
  * @param  {Object} entity the entity to check intersections against
- * @param  {Number} index 
+ * @param  {Number} index
  * @return {Array} intersections : the list of intersections
  */
 export default function intersect (ray, entity, index) {
@@ -23,9 +23,13 @@ export default function intersect (ray, entity, index) {
 
   let min = vec3.transformMat4(mat4.identity([]), tmpMin, modelMat)
   let max = vec3.transformMat4(mat4.identity([]), tmpMax, modelMat)
-  console.log(entity.bounds, entity.bounds.min, entity.bounds.max)
+  //console.log(modelMat)//entity.bounds, entity.bounds.min, entity.bounds.max)
+  console.log(min, max)
 
   const bounds = [min, max] // [entity.bounds.min, entity.bounds.max] // FIXME !!!! bounds are in local coordinates, not world coordinates !
+
+  //FIXME : haaaack
+  entity.meta.selected = false
 
   const hitAABB = intersectAABB([], ray.ro, ray.rd, bounds)
   if (hitAABB) {
@@ -37,16 +41,18 @@ export default function intersect (ray, entity, index) {
     const localRayRo = transformMat4(vec3.create(), ray.ro, invModelMat)
     const localRayRd = transformMat4(vec3.create(), ray.rd, invModelMat)
 
+
     //if we do not want to go any deeper into the object
-    if(entity.meta.pickLimit && entity.meta.pickLimit === 'bounds')
+    if(!entity.meta.pickLimit || (entity.meta.pickLimit && entity.meta.pickLimit === 'bounds'))
     {
       const hitPoint = hitAABB
-      console.log('that is a match !! , for ' + entity.id)
+      console.log('that is a match !! , for ' + entity.meta.id)
       // distance between intersect point and ray origin, in world space
       const distance = vec3.length(vec3.subtract(vec3.create(), hitPoint, ray.ro))
 
-      setSelection(entity)
-      return {intersect: {pos: hitPoint, distance}, entity, index}
+      entity.meta.selected = true
+      //setSelection(entity)
+      return {intersect: {pos: hitPoint, distance}, entity}
     }
 
     if (!entity.geometry.cells) {
