1e32a79efbc5fcf3468d152559a8cabb4f003484
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-03-13 14:36:19 +0100

    chore(picksStream): adjusted mouse/pointer coordinate computation

--- src/utils/picking/picksStream.js
+++ src/utils/picking/picksStream.js
@@ -12,14 +12,14 @@ function withLatestFrom (fn, sampleStream$, otherStreams) {
 
 export default function picksStream (taps$, viewport$, camera$, entities$) {
   return withLatestFrom(function (taps, viewport, camera, entities) {
-    return pickAttempt({position: [taps.clientX-viewport.bRect.left, taps.clientY-viewport.bRect.top]}, [viewport.bRect.left, viewport.bRect.top, viewport.bRect.width, viewport.bRect.height], camera, entities)
+    return pickAttempt({position: [taps.clientX - viewport.bRect.left, taps.clientY - viewport.bRect.top]}, [viewport.bRect.left, viewport.bRect.top, viewport.bRect.width, viewport.bRect.height], camera, entities)
   }, taps$, [viewport$, camera$, entities$])
 }
 
 function pickAttempt (pointer, viewport, camera, entities) {
-  //viewport = [ 0, 0, viewport[2], viewport[3] ]
+  // viewport = [ 0, 0, viewport[2], viewport[3] ]
   pointer.position[1] = viewport[3] - pointer.position[1]
-  //console.log(pointer.position, viewport)
+  // console.log(pointer.position, viewport)
 
   // warning !!! posible issues with camera-unproject , itself used in other modules https://github.com/Jam3/camera-unproject/issues/1
   const {projection, view} = camera
