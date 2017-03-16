9f4b88c79d1014884a3a554a61e18a39f905bed6
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-03-16 22:43:12 +0100

    refactor(withTooltip): updated withToolTip to new tooltip system

--- src/ui/widgets/utils.js
+++ src/ui/widgets/utils.js
@@ -1,8 +1,19 @@
+import {span, label} from '@cycle/dom'
 
 export function withToolTip (item, tooltip = '', tooltipPos = 'bottom') {
   let moddedItem = item
-  item.data = {...item.data, attrs: {'data-tooltip': tooltip}}
-  item.sel = `${item.sel}.tooltip-${tooltipPos}`
+  console.log('withToolTip', item)
+  // item.data = {...item.data, attrs: {'data-tooltip': tooltip}}
+  item.sel = `${item.sel}.tooltipWrapper`// `${item.sel}.tooltip-${tooltipPos}`
+  const tooltipElement = span(`.tooltip.${tooltipPos}`, [tooltip])
+  let children = item.children? item.children: []
+  if(item.text){
+    children.push(label(item.text))
+  }
+  children.push(tooltipElement)
+  item.children = children
+  //item.children = item.children ? [...item.children, tooltipElement] : [tooltipElement]
+
   // item.props = {...item.props, className:`tooltip-${tooltipPos}`}
   return item
 }
