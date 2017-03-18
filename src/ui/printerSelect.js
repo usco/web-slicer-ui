0a2de906883beac243741f4bfb0ba599651250dc
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-03-18 22:32:01 +0100

    chore(): removed useless file

--- src/ui/printerSelect.js
+++
@@ -1,4 +0,0 @@
-import classNames from 'classnames'
-import { html } from 'snabbdom-jsx'
-import {domEvent, fromMost, makeStateAndReducers$, makeDefaultReducer, toMost} from '../utils/cycle'
-import { div, button, li, ul, h2 } from '@cycle/dom'
