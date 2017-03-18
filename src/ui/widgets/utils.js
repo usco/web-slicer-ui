52b99d641305fb911ed54b0d3da9446fbef88dac
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-03-18 22:28:34 +0100

    refactor(ui utils): cleanups

--- src/ui/widgets/utils.js
+++ src/ui/widgets/utils.js
@@ -2,17 +2,16 @@ import {span, label} from '@cycle/dom'
 
 export function withToolTip (item, tooltip = '', tooltipPos = 'bottom') {
   let moddedItem = item
-  console.log('withToolTip', item)
   // item.data = {...item.data, attrs: {'data-tooltip': tooltip}}
   item.sel = `${item.sel}.tooltipWrapper`// `${item.sel}.tooltip-${tooltipPos}`
   const tooltipElement = span(`.tooltip.${tooltipPos}`, [tooltip])
-  let children = item.children? item.children: []
-  if(item.text){
+  let children = item.children ? item.children : []
+  if (item.text) {
     children.push(label(item.text))
   }
   children.push(tooltipElement)
   item.children = children
-  //item.children = item.children ? [...item.children, tooltipElement] : [tooltipElement]
+  // item.children = item.children ? [...item.children, tooltipElement] : [tooltipElement]
 
   // item.props = {...item.props, className:`tooltip-${tooltipPos}`}
   return item
