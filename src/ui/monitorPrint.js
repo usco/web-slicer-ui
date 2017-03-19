7ff64a0dbe436e26773441177c998589adc72f92
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-03-20 00:01:09 +0100

    refactor(monitorPrint): restructured and cleaned up UI

--- src/ui/monitorPrint.js
+++ src/ui/monitorPrint.js
@@ -1,9 +1,10 @@
-import {find, propEq, lensPath, compose, view as rView} from 'ramda'
-import {section, div, button, img} from '@cycle/dom'
+import {find, propEq, lensPath, compose, view as rView, nth} from 'ramda'
+import {section, div, button, img, table, tr, td, h1, span, li ,ul} from '@cycle/dom'
 import renderProgressBar from './widgets/ProgressBar'
 
 import {domEvent, makeStateAndReducers$} from '../utils/cycle'
 import {hotends, bed, jobInfos, formatTime} from '../core/printing/utils'
+import {formatNumberTo} from '../utils/formatters'
 
 const init = (state) => {
   console.log('init monitorPrint state', state)
@@ -17,32 +18,36 @@ export const actions = {
 
 const view = function (state) {
   const activePrinter = find(propEq('id', state.printing.activePrinterId))(state.printing.printers)
-
   // we consider the printer to be inactive if the state is undefined or idle
   const isInactive = false// (state.printing.printerStatus.state === undefined || state.printing.printerStatus.state === 'idle')
+  const isAvailable = activePrinter && activePrinter.infos
 
   const hTemps = hotends(activePrinter)
     .map((hotend, index) => `T${index}: ${hotend.temperature.current} / ${hotend.temperature.target} °C`)
   const bedTemp = `Bed: ${bed(activePrinter).temperature.current} / ${bed(activePrinter).temperature.target} °C`
 
-  // only show detailed status if printer is active
-  const statusUi = isInactive ? null : div('.temperatures', `Temperature: ${hTemps}, ${bedTemp}`)
-
   const progressData = jobInfos(state.printing.printerStatus)
   const timeLeft = formatTime(progressData.time_total - progressData.time_elapsed)
-  const progressUi = isInactive
-    ? null
-    : div('.printProgress', [div('', `Time left: ${timeLeft.hours}h ${timeLeft.minutes}min`)])
+  const percent = formatNumberTo(progressData.progress, 2)// progress percent in 0-1 range, to 2 decimals
+
+  // only show detailed status if printer is active
+  const temperaturesUi = isInactive ? undefined : ul('.temperatures', [li(nth(0, hTemps)), li(nth(1, hTemps)), li(bedTemp)])
+  const progressTimeUi = isInactive ? undefined : div('.timeLeft', `Time left: ${timeLeft.hours}h ${timeLeft.minutes}min`)
+  const progressBarColor = percent < 1 ? undefined : 'green'
+
+  const pauseResumeToggle = button('.pauseResume', state.print.paused ? 'play' : 'pause')
+
+  const controlLine = isAvailable ? div([pauseResumeToggle, button('.abort', 'abort')]) : undefined
+
+  console.log(activePrinter, controlLine)
 
   return section('.MonitorPrint', [
-    div('.printStatus', 'STATUS:' + state.printing.printerStatus.message),
-    statusUi,
-    progressUi,
-    renderProgressBar({progress: progressData.progress * 100, hideOnDone: false}),
-    div('', [
-      button('.pauseResume', state.print.paused ? 'play' : 'pause'),
-      button('.abort', 'abort')
-    ]),
+    renderProgressBar({progress: percent, hideOnDone: false, color: progressBarColor}),
+    div('.progressPercent', `${percent*100}%`),
+    h1('.printStatus', state.printing.printerStatus.message),
+    progressTimeUi,
+    temperaturesUi,
+    controlLine,
     img('.printerCameraFrame', {props: {src: state.printing.frame ? state.printing.frame : ''}})
   ])
 }
