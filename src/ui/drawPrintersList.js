d8c6f33165ec957529ad11df334af56fd9657673
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-03-19 17:56:45 +0100

    feat(printersList): - added display of friendlyNames when available - updated & cleaned up structure

--- src/ui/drawPrintersList.js
+++ src/ui/drawPrintersList.js
@@ -1,10 +1,9 @@
 import classNames from 'classnames'
-import { div, span, button, li, ul, h2, section, table, th, tr, td } from '@cycle/dom'
+import { div, span, button, li, ul, h2, section, table, th, tr, td, label } from '@cycle/dom'
 import {propEq, find} from 'ramda'
 
 import {videoIconSvg, playIconSvg, pauseIconSvg} from './widgets/icons'
 import icon from './widgets/icon'
-
 
 export default function drawPrintersList (state) {
   const printers = table('.printersList', state.printing.printers
@@ -12,32 +11,48 @@ export default function drawPrintersList (state) {
       const isSelected = state.printing.activePrinterId === printer.id
       const isClaimed = printer.claimed
       const isPaused = true
-      const printerText = isSelected ? `${printer.name} ${state.printing.printerStatus.message}` : printer.name
+      const activePrinter = find(propEq('id', state.printing.activePrinterId))(state.printing.printers)
+
+      const printerName = printer.friendlyName ? printer.friendlyName : printer.name
+      const printerText = isSelected ? `${printerName} ${state.printing.printerStatus.message}` : printerName
 
       const classes = classNames({'.selected': isSelected, '.printerL': true})
 
-      const claimButtons = isClaimed // buttons to claim / unclaim printer
+      /*const claimButtons = isClaimed // buttons to claim / unclaim printer
         ? button('.unClaim .claimed', {attrs: {'data-id': printer.id}}, 'unClaim')
-        : button('.claim', {attrs: {'data-id': printer.id}}, 'claim')
+        : button('.claim', {attrs: {'data-id': printer.id}}, 'claim')*/
+      const claimToggler = isClaimed // buttons to claim / unclaim printer
+        ? td('.unClaim .claimed', {attrs: {'data-id': printer.id}}, 'unClaim')
+        : td('.claim', {attrs: {'data-id': printer.id}}, 'claim')
 
       const pauseResumeButton = isClaimed ? (isPaused ? icon(playIconSvg) : icon(pauseIconSvg)) : ''
       const videoFramesButton = isClaimed ? icon(videoIconSvg) : ''
 
       return tr(classes, {attrs: {'data-id': printer.id}}, [
         td([printerText]),
+        td(['connected']),
         td([pauseResumeButton]),
 
         td([videoFramesButton]),
-        td([claimButtons])
 
+        td(['190°C']),
+        td(['205°C']),
+        td(['65°C']),
+
+        td([
+          span('.gnok', 'foobar')
+        ]),
+        claimToggler
       ])
     })
-  )// printerStatus
+  )
+  const noPrinters = table('.printersList', [
+    tr([td('please wait fetching printers')])
+  ])
   const printerSetup = section('', [
-    state.printing.printers.length > 0 ? div('Select printer', [printers]) : span('please wait, fetching printers ...')
+    state.printing.printers.length > 0 ? printers : noPrinters
   ])
 
-  const activePrinter = find(propEq('id', state.printing.activePrinterId))(state.printing.printers)
   // console.log(activePrinter, state.activePrinter)
   const newPrintDisabled = state.buildplate.entities.length === 0 || state.printing.printerStatus.busy === true
   return printerSetup
