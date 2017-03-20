2b27f8c8d0b219f29c111c66e01df49004f23f9f
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-03-20 11:15:19 +0100

    feat(progressbar): - color is now settable via prop - percents are now in the 0-1 range instead of 0-100

--- src/ui/widgets/ProgressBar/index.js
+++ src/ui/widgets/ProgressBar/index.js
@@ -4,12 +4,13 @@ require('./style.css')
 export default function renderProgressBar (state) {
   const defaults = {
     progress: 0,
-    hideOnDone: true
+    hideOnDone: true,
+    color: 'rgb(0, 178, 255)'
   }
-  const {progress, hideOnDone} = Object.assign({}, defaults, state)
+  const {progress, hideOnDone, color} = Object.assign({}, defaults, state)
 
-  const element = (hideOnDone && progress === 100)
+  const element = (hideOnDone && progress === 1)
     ? null
-    : div('.progressBar', [span('.fill', { style: { width: `${progress}%` } })])
+    : div('.progressBar', [span('.fill', { style: { width: `${progress * 100}%`, background: color } })])
   return element
 }
