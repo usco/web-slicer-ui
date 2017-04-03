1a28ff55781e58598d6c2202e52fa727bfab16df
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2017-04-03 13:17:42 +0200

    chore(icon): modified html structure for icons

--- src/ui/widgets/icon.js
+++ src/ui/widgets/icon.js
@@ -1,5 +1,5 @@
 import {button, i, div} from '@cycle/dom'
 
 export default function icon (svg) {
-  return div([i({props: {innerHTML: svg}})])
+  return div('.icon', [i({props: {innerHTML: svg}})])
 }
